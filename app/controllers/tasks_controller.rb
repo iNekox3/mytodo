8186ecc787e949d8a5414399fc05ab44aef0299c
iNekox3 <inekox3@hotmail.com>
2019-05-14 11:27:05 +0800

    Implement basic add and delete task functionalities

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,23 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_back fallback_location: root_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_back fallback_location: root_path
+  end
+
+  private def task_params
+    params.require(:task).permit(:taskname)
+  end
+
+end
