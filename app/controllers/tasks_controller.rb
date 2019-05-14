f986f1a0bb25625fd4bde6f8ccf40b94c4e7c177
iNekox3 <inekox3@hotmail.com>
2019-05-14 11:26:03 +0800

    Implement delete task functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,6 +10,12 @@ class TasksController < ApplicationController
     redirect_back fallback_location: root_path
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_back fallback_location: root_path
+  end
+
   private def task_params
     params.require(:task).permit(:taskname)
   end
