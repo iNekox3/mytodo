cde6cc64bc9c4a3f6115d82766b9aa6aaf67671a
iNekox3 <inekox3@hotmail.com>
2019-05-14 10:55:50 +0800

    Implement add task functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,17 @@
 class TasksController < ApplicationController
 
   def index
-    #@tasks = Task.all
+    @tasks = Task.all
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_back fallback_location: root_path
+  end
+
+  private def task_params
+    params.require(:task).permit(:taskname)
   end
 
 end
